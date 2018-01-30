0241095fb2589c1e1a614648660d4b93b6550af3
Aapo Vienamo <aapo.vienamo@iki.fi>
2018-01-30 23:03:57 +0200

    boards: Add support for nRF52 Thingy52 (PCA20020)

---
+++ boards/arm/nrf52_pca20020/board.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2016 Nordic Semiconductor ASA.
+ * Copyright (c) 2018 Aapo Vienamo
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef __INC_BOARD_H
+#define __INC_BOARD_H
+
+#include <soc.h>
+
+/* Push button switch 0 */
+#define SW0_GPIO_PIN	11
+#define SW0_GPIO_NAME	CONFIG_GPIO_NRF5_P0_DEV_NAME
+
+#endif /* __INC_BOARD_H */
